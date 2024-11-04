d278ac3396c6b89415f6e22ea3801930790d0506
GustavoHerb <141774477+GustavoHerb@users.noreply.github.com>
2024-11-04 18:50:21 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,107 @@
+# contador = 0
+# inicio = 0
+# fim = 11
+# passo = 2
+
+# for i in range(inicio,fim,passo):
+#   print(f"Indice={contador} | Valor= {i}")
+#   contador += 1
+
+# meu_set = {1, 2 ,3,  4}
+
+# print(type(meu_set))
+
+# ATIVIDADE 1
+
+# while True:
+  # pergunta = int(input("Você quer sair? Digite 1 para sim, digite 0 para não: "))
+  # if pergunta == 1:
+  #   print("Saindo do programa!")
+  #   break
+  # else: 
+  #   print("Continuando!")
+    
+  # usuarioidade = int(input("Digite a idade de usuario: "))
+  
+  # if usuarioidade < 12:
+  #   print("Você é criança!")
+  # elif usuarioidade >= 12 and usuarioidade <= 17:
+  #   print("Adolescente")
+  # elif usuarioidade >= 18 and usuarioidade <= 59:
+  #   print("Adulto")
+  # else: 
+  #   print("Idoso")
+
+
+
+# ATIVIDADE 2
+
+# while True: 
+#   pergunta = int(input("Você quer sair? Digite 1 para sim, digite 0 para não: "))
+#   if pergunta == 1:
+#     print("Saindo do programa!")
+#     break
+#   else: 
+#     print("Continuando!")
+
+#   numero1 = int(input("Digite o primeiro numero: "))
+#   numero2 = int(input("Digite o segundo numero: "))
+#   numero3 = int(input("Digite o terceiro numero: "))
+
+#   if numero1 > numero2 and numero1 > numero3:
+#     print(f"O maior numero é: {numero1}")
+  
+#   if numero2 > numero1 and numero2 > numero3:
+#     print(f"O maior numero é: {numero2}")
+    
+    
+#   if numero3 > numero2 and numero3 > numero1:
+#     print(f"O maior numero é: {numero3}")
+
+
+# ATIVIDADE 3
+
+# c = 0
+# numero_lista = []
+# qtd_par = 0
+# qtd_impar = 0
+
+
+# while True: 
+#   pergunta = int(input("Você quer sair? Digite 1 para sim, digite 0 para não: "))
+#   if pergunta == 1:
+#     print("Saindo do programa!")
+#     break
+#   else: 
+#     print("Continuando!")
+#   for i in range(1,11): 
+#     numero = int(input("Digite um numero: "))
+#     numero_lista.insert(c, numero)
+#     c += 1     
+#     if numero % 2 == 0:
+#       qtd_par += 1 
+#       print("O numero é par")
+#     else: 
+#       qtd_impar += 1
+#       print("O numero é impar!")
+
+
+#   print(numero_lista)
+
+# print(f"A quantidad de numero par foi de {qtd_par}, já a quantidade de impar foi: {qtd_impar}")
+
+
+
+# ATIVIADE 4
+# qtd_pessoas = int(input("Quantas pessoas querem ver a idade?"))
+# c = 0
+# n = 0
+# soma = 0
+# while c < qtd_pessoas :
+#   n = int(input("Digite sua idade: "))
+#   soma += n
+#   c += 1 
+#   media = soma / qtd_pessoas
+
+# print(f" a media foi: {media}")
+
